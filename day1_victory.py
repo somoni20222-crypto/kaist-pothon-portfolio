1a6aaa32f0075cf0935ef7ed6e5df661c7440073
somoni20222-crypto <somoni.20222@icloud.com>
2026-04-26 02:28:22 +0500

    Create day1_victory.py

---
+++ day1_victory.py
@@ -0,0 +1,23 @@
+# Хадиса Сомонӣ - KAIST 2026
+# 2:17 AM, Душанбе. Ман хоб накардам, чунки орзу дорам
+
+print("=" * 40)
+print("ПОРТФОЛИОИ ХАДИСА БАРОИ KAIST")
+print("=" * 40)
+
+name = "Хадиса"
+city = "Душанбе" 
+university = "KAIST"
+time_now = "2:17 AM"
+
+print(f"Номи ман: {name}")
+print(f"Аз куҷо: {city}, Тоҷикистон 🇹🇯")
+print(f"Орзуи ман: {university}")
+print(f"Соат {time_now} аммо ман код мезанам")
+
+print("\nМан 404-ро шикастам.")
+print("Ман Footer-ро шикастам.")
+print("Ман GitHub сохтам.")
+print("МАН БА KAIST МЕРАВАМ!")
+
+print("=" * 40
